bd711e9fa11360f1554b142fb26ac66bd11d6236
Justin Bois <bois@caltech.edu>
2017-06-01 23:19:47 -0700

    Updated 2017 website.

---
+++ 2017/lessons/zen.py
@@ -0,0 +1,5 @@
+# The first few lines from The Zen of Python by Tim Peters
+print('Beautiful is better than ugly.')
+print('Explicit is better than implicit.')
+print('Simple is better than complex.')
+print('Complex is better than complicated.')
