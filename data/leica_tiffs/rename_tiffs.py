ab047348e71110b1aaab5e60565479cedb8bfbf4
justinbois <bois@caltech.edu>
2020-06-25 00:36:56 -0700

    removed rename_tiffs.py script

--- data/leica_tiffs/rename_tiffs.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-prefix = 'stage9_Series006_t'
-suffix = '_z000_ch00.tif'
-
-for i in range(125):
-    fname = prefix + ('%02d' % i) + suffix
-    subprocess.call(['cp', fname, 'leica_tiffs/'])
